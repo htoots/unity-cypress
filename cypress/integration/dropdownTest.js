ce707e83311b8524a92795967c2707e03a4696b7
Hannes Toots <htoots@ttu.ee>
2020-11-13 17:04:05 +0200

    refactor tests

--- cypress/integration/dropdownTest.js
+++ cypress/integration/dropdownTest.js
@@ -1,17 +1,17 @@
-describe('DropDownTest', () => {
+describe('Dropdown test', () => {
 
     before(() => {
       cy.visit("/dropdown");
     });
 
     // Test 1: Page loads and contains necessary information (dropdown)
-    it('should confirm page', () => {
+    it('can get the correct page', () => {
       cy.get('h3').contains('Dropdown List');
     });
 
     // Test 2: Dropdown holds expected information
-    it('should check dropdown for existing options', () => {
-      cy.get('#dropdown').within(() => {
+    it('can get the dropdown and confirm it', () => {
+      cy.get('select[id=dropdown]').within(() => {
         cy.get('option[selected=selected]').contains('Please select an option');
         cy.get('option[value=1]').contains('Option 1');
         cy.get('option[value=2]').contains('Option 2');
@@ -20,16 +20,16 @@ describe('DropDownTest', () => {
 
     
     // Test 3: Able to click on both dropdown options
-    it('should be able to click on options', () => {
-      cy.get('#dropdown').select('Option 1').should('have.value', '1');
-      cy.get('#dropdown').select('Option 2').should('have.value', '2');
+    it('can click on the two options', () => {
+      cy.get('select[id=dropdown]').select('Option 1').should('have.value', '1');
+      cy.get('select[id=dropdown]').select('Option 2').should('have.value', '2');
     });
     
     // Test 4: Unable to click on placeholder option, value from before is retained
     // Cypress does not allow clicking on placeholder option, code checks if value is disabled.
-    it('should not be able to click on disabled option', () => {
-      cy.get('#dropdown').within(() => {
+    it('can not click on the disabled option', () => {
+      cy.get('select[id=dropdown]').within(() => {
         cy.get('option[disabled=disabled]').contains('Please select an option');
-      })
+      });
     });
   });
