24360b25f60de88a703201be9bae376405b88fb6
Levent Alici <mealic@taltech.ee>
2020-11-14 19:40:40 +0200

    added test2

--- cypress/integration/redirectionTest.js
+++ cypress/integration/redirectionTest.js
@@ -7,7 +7,18 @@ describe('Redirection test', () => {
     it('can redirect to the status codes page', () => {
         cy.url().should('eq', 'https://the-internet.herokuapp.com/status_codes');
     });
-
+    //Test 2: Check if redirected page returns 302
+    it('check URL without redirection', () =>
+    {
+        cy.request({
+            url: 'https://the-internet.herokuapp.com/redirect',
+            followRedirect: false
+        })
+            .then((resp) => {
+                // redirect status code is 302
+                expect(resp.status).to.eq(302)
+            })
+    });
 
 
 });
