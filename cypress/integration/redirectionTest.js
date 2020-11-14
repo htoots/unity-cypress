65152ad8cfbc45005de6d4e9c8aeac02dd21ff38
Levent Alici <mealic@taltech.ee>
2020-11-14 23:21:43 +0200

    fixed header test

--- cypress/integration/redirectionTest.js
+++ cypress/integration/redirectionTest.js
@@ -7,27 +7,23 @@ describe('Redirection test', () => {
     it('can redirect to the status codes page', () => {
         cy.url().should('eq', 'https://the-internet.herokuapp.com/status_codes');
     });
-    //Test 2: Check if redirected page returns 302
-    it('check URL without redirection', () =>
+    //Test 2: Check Header
+    it('should return header correctly', () =>
     {
-        cy.request({
-            url: 'https://the-internet.herokuapp.com/redirect',
-            followRedirect: false
-        })
-            .then((resp) => {
-                // redirect status code is 302
-                expect(resp.status).to.eq(302)
-            })
+        cy.get('h3').should("have.text", "Status Codes");
+
     });
+
+
     //Test 3: Check status 200
     it('should return status 200', () => {
+        cy.visit("/redirect");
         cy.contains('200').click().url().should('eq', 'https://the-internet.herokuapp.com/status_codes/200');
     });
     //Test 4: Check status 404
     it('should return status 404', () => {
-        //need to revisit the previous page as it is no longer on status codes home page
         cy.visit("/redirect");
         cy.contains('404').click().url().should('eq', 'https://the-internet.herokuapp.com/status_codes/404');
     });
 
-});
+})
